86256676bcf5408d409f13e60f820079f3b9d34f
BuddyCodezgit config --global user.email riddhi990@outlook.comgit config --global user.name BuddyCodez <92288214+BuddyCodez@users.noreply.github.com>
2023-10-15 21:37:18 +0530

    site veirifcation

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,9 @@ export default function RootLayout({
 
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<head />
+			<head>
+				<meta name="google-site-verification" content="d8Q9bSGyoBL8RSathwiLAJd3qbQhUcl_au7udJd5XZo" />
+			</head>
 			<body
 				
 				className={clsx(
